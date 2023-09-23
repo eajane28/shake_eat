6ce1d25e53667d5a041c5bf3cf4bb4c6c2dd5e21
Rhea Jane Escalona <rheajeaj@gmail.com>
2023-09-23 18:15:11 +0800

    Share view done

--- lib/ui/views/share/share_view.dart
+++ lib/ui/views/share/share_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'share_viewmodel.dart';
 
@@ -16,47 +17,156 @@ class ShareView extends StackedView<ShareViewModel> {
         body: SafeArea(
       child: Stack(
         children: [
-          Column(children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 138.0,
-              decoration: const BoxDecoration(
-                color: Color(0xFFFBAB10),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 138.0,
+            decoration: const BoxDecoration(
+              color: Color(0xFFFBAB10),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+            ),
+            child: Row(children: [
+              const SizedBox(width: 30.0),
+              GestureDetector(
+                  onTap: null,
+                  child: const Icon(Icons.chevron_left,
+                      color: Colors.white, size: 40.0)),
+              const Text(
+                'Share to',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-              child: Row(children: [
-                const SizedBox(width: 30.0),
-                GestureDetector(
-                    onTap: null,
-                    child: const Icon(Icons.chevron_left,
-                        color: Colors.white, size: 40.0)),
-                const Text(
-                  'Share to',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w600,
+            ]),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 96.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: ClipRRect(
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 18.0),
+                  padding: const EdgeInsets.only(top: 96.0),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
-                ),
-              ]),
-            ),
-            Positioned(
-              bottom: 20.0,
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 18.0),
-                padding: const EdgeInsets.only(top: 96.0),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height -176,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(30.0),
+                  child: Stack(children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                height: 44.22,
+                                width: 46.9,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Stack(
+                                  children: [
+                                    Center(
+                                      child: SvgPicture.asset('assets/messenger.svg'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              const Text('Message',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                height: 44.22,
+                                width: 46.9,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Stack(
+                                  children: [
+                                    Center(
+                                      child: SvgPicture.asset('assets/group.svg')
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              const Text('Group',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                height: 44.22,
+                                width: 46.9,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Stack(
+                                  children: [
+                                    Center(
+                                      child: SvgPicture.asset('assets/link.svg')
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              const Text('Copy link',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                height: 44.22,
+                                width: 46.9,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Stack(
+                                  children: [
+                                    Center(
+                                      child: SvgPicture.asset('assets/more.svg')
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              const Text('More',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ]),
                 ),
               ),
             ),
-          ]),
+          ),
         ],
       ),
     ));
