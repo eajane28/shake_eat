c65ab543be687a7606f200a46282d1ba675bb592
cristopher <emailcristopher@gmail.com>
2023-09-23 20:30:07 +0800

    Make the distance a slider instead of checkbox

--- lib/ui/dialogs/info_alert/distance_dialog/distance_dialog_ui.dart
+++ lib/ui/dialogs/info_alert/distance_dialog/distance_dialog_ui.dart
@@ -1,186 +1,128 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter/src/foundation/change_notifier.dart';
-// import 'package:flutter/src/widgets/framework.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-import 'distance_dialog_viewmodel.dart';
+class DistanceDialogViewModel extends BaseViewModel {
+  double sliderValue = 1;
 
-class DistanceDialogUi extends StackedView<DistanceDialogViewModel> {
-  DistanceDialogUi({
-    super.key,
-    required this.request,
-    required this.completer,
-  });
+  void updateSliderValue(double newValue) {
+    sliderValue = newValue;
+    notifyListeners();
+  }
+}
 
+class DistanceDialogUi extends StatelessWidget {
   final DialogRequest request;
   final Function(DialogResponse) completer;
-  bool? value = false;
+
+  const DistanceDialogUi({
+    Key? key,
+    required this.request,
+    required this.completer,
+  }) : super(key: key);
 
   @override
-  Widget builder(
-      BuildContext context, DistanceDialogViewModel viewModel, Widget? child) {
-    Color getColor(Set<MaterialState> states) {
-      const Set<MaterialState> interactiveStates = <MaterialState>{
-        MaterialState.selected,
-        MaterialState.focused,
-        MaterialState.pressed,
-      };
-      if (states.any(interactiveStates.contains)) {
-        return Colors.orange;
-      }
-      return Colors.white;
-    }
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<DistanceDialogViewModel>.reactive(
+      viewModelBuilder: () => DistanceDialogViewModel(),
+      builder: (context, model, child) {
+        double maxSlider = 20;
+        String distanceLabel = 'Near'; // Default label
 
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      backgroundColor: const Color(0xFFFBAB10),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Text(
-                request.title!,
-                style: const TextStyle(
-                    color: Color(0xFFDA1D1D),
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                children: [
-                  const SizedBox(width: 30.0),
-                  SizedBox(
-                    height: 16,
-                    width: 16,
-                    child: ColoredBox(
-                      color: Colors.white,
-                      child: Checkbox(
-                        checkColor: Colors.black,
-                        fillColor: MaterialStateProperty.resolveWith(getColor),
-                        value: value,
-                        onChanged: (bool? value) {
-                          value = !value!;
-                        },
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 9.0),
-                  const Text("Nearby",
-                      style: TextStyle(
+        if (model.sliderValue <= maxSlider/3) {
+          distanceLabel = 'Near';
+        } else if (model.sliderValue <= (maxSlider/3)*2){
+          distanceLabel = 'Moderate';
+        } else {
+          distanceLabel = 'Far';
+        }
+
+        return Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          backgroundColor: const Color(0xFFFBAB10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text(
+                    request.title!,
+                    style: const TextStyle(
                         color: Color(0xFFDA1D1D),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                      ))
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                children: [
-                  const SizedBox(width: 30.0),
-                  SizedBox(
-                    height: 16,
-                    width: 16,
-                    child: ColoredBox(
-                      color: Colors.white,
-                      child: Checkbox(
-                        checkColor: Colors.black,
-                        fillColor: MaterialStateProperty.resolveWith(getColor),
-                        value: value,
-                        onChanged: (bool? value) {
-                          value = !value!;
-                        },
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 30.0),
+                      Text(
+                        distanceLabel, // Display the dynamically determined label
+                        style: const TextStyle(
+                          color: Color(0xFFDA1D1D),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                  const SizedBox(width: 9.0),
-                  const Text("Moderate",
-                      style: TextStyle(
-                        color: Color(0xFFDA1D1D),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                      ))
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                children: [
-                  const SizedBox(width: 30.0),
-                  SizedBox(
-                    height: 16,
-                    width: 16,
-                    child: ColoredBox(
-                      color: Colors.white,
-                      child: Checkbox(
-                        checkColor: Colors.black,
-                        fillColor: MaterialStateProperty.resolveWith(getColor),
-                        value: value,
-                        onChanged: (bool? value) {
-                          value = !value!;
-                        },
-                      ),
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Slider(
+                    value: model.sliderValue,
+                    onChanged: (double newValue) {
+                      model.updateSliderValue(newValue);
+                    },
+                    min: 1,
+                    max: maxSlider,
+                    divisions: 19,
                   ),
-                  const SizedBox(width: 9.0),
-                  const Text("Far",
-                      style: TextStyle(
-                        color: Color(0xFFDA1D1D),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w600,
-                      ))
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child:
-                      // viewModel.isBusy
-                      //     ? const Center(
-                      //         child: CircularProgressIndicator(
-                      //           color: Colors.white,
-                      //         ),
-                      //       )
-                      //     :
-                      const Text(
-                    'Save',
-                    style: TextStyle(
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text(
+                    '${model.sliderValue.toStringAsFixed(0)} km less', // Display the slider value
+                    style: const TextStyle(
                       color: Color(0xFFDA1D1D),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                      fontSize: 18.0,
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Container(
+                      height: 50,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        'Save',
+                        style: TextStyle(
+                          color: Color(0xFFDA1D1D),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
-  }
-
-  @override
-  DistanceDialogViewModel viewModelBuilder(BuildContext context) {
-    return DistanceDialogViewModel();
   }
 }
