c2c940fb3e083d4266b81a3482bef5b4bfe8dfcb
cristopher <emailcristopher@gmail.com>
2023-10-06 18:23:17 +0800

    add toast message for first launch dummy task

--- lib/persistent_data.dart
+++ lib/persistent_data.dart
@@ -1,5 +1,6 @@
 // This file contains all the persistent data
 import 'package:flutter/foundation.dart';
+import 'package:food_frenzy/util/toast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PersistentData {
@@ -13,10 +14,15 @@ class PersistentData {
       print('first launch: ');
       print(isFirstLaunch ?? "true");
     }
-    if (kDebugMode) {
+
+    // first launch
+    if (isFirstLaunch == null) {
       // todo
       // add a task what to do when it is first launch
-      await prefs.setBool('isFirstLaunch', false);
+      showToastMessage("First Launch");
+      await prefs.setBool('isFirstLaunch', false); // set first launch to false
+    }else {
+      showToastMessage("Not first launch");
     }
   }
 }
