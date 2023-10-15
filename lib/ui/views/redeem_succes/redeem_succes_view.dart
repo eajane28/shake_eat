8d960a6ee4c1b543d6509e2cf7fed58690ff2c4e
Rhea Jane Escalona <rheajeaj@gmail.com>
2023-10-15 15:39:21 +0800

    maps view added

--- lib/ui/views/redeem_succes/redeem_succes_view.dart
+++ lib/ui/views/redeem_succes/redeem_succes_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:stacked/stacked.dart';
 
+import '../widgets/panel_widget.dart';
 import 'redeem_succes_viewmodel.dart';
 
 class RedeemSuccesView extends StackedView<RedeemSuccesViewModel> {
@@ -12,128 +14,127 @@ class RedeemSuccesView extends StackedView<RedeemSuccesViewModel> {
     RedeemSuccesViewModel viewModel,
     Widget? child,
   ) {
+    final panelHeightClosed = MediaQuery.of(context).size.height * .05;
+
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: GestureDetector(
-            onTap: null,
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 405,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFDA1D1D),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50.0),
-                          topRight: Radius.circular(50.0),
-                          bottomLeft: Radius.circular(150.0),
-                          bottomRight: Radius.circular(150.0),
+      body: SlidingUpPanel(
+        minHeight: panelHeightClosed,
+        maxHeight: MediaQuery.of(context).size.height * .9,
+        margin: const EdgeInsets.symmetric(horizontal: 22),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: GestureDetector(
+              onTap: null,
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 405,
+                        decoration: const BoxDecoration(
+                          color: Color(0xFFDA1D1D),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(50.0),
+                            topRight: Radius.circular(50.0),
+                            bottomLeft: Radius.circular(150.0),
+                            bottomRight: Radius.circular(150.0),
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 65.0),
-                      child: Column(
-                        children: [
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 4.0),
-                                  width: 61,
-                                  height: 6,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(5.0),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 65.0),
+                        child: Column(
+                          children: [
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 4.0),
+                                    width: 61,
+                                    height: 6,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(5.0),
+                                    ),
                                   ),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 4.0),
-                                  width: 61,
-                                  height: 6,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(5.0),
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 4.0),
+                                    width: 61,
+                                    height: 6,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(5.0),
+                                    ),
                                   ),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 4.0),
-                                  width: 61,
-                                  height: 6,
-                                  decoration: BoxDecoration(
-                                    color: const Color(0xFFFBAB10),
-                                    borderRadius: BorderRadius.circular(5.0),
+                                  Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 4.0),
+                                    width: 61,
+                                    height: 6,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFFFBAB10),
+                                      borderRadius: BorderRadius.circular(5.0),
+                                    ),
                                   ),
-                                ),
-                              ]),
-                          const Padding(
-                            padding: EdgeInsets.only(top: 25.0),
-                            child: Text(
-                              'Claimed!',
-                              style: TextStyle(
-                                fontSize: 36,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 250.0),
-                      child: Column(
-                        children: [
-                          const Center(
-                            child: Text(
-                              'CONGRATULATIONS!',
-                              style: TextStyle(
-                                fontSize: 40,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 165),
-                          Center(
-                            child: SizedBox(
-                              height: 300,
-                              child: Image.asset('assets/collected.png',
-                                  fit: BoxFit.contain),
-                            ),
-                          ),
-                          const SizedBox(height: 70),
-                          Column(
-                            children: [
-                              Image.asset(
-                                'assets/Vector.png',
-                              ),
-                              const Text(
-                                'Let\'s eat!',
+                                ]),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 25.0),
+                              child: Text(
+                                'Claimed!',
                                 style: TextStyle(
-                                  fontSize: 17,
-                                  color: Color(0xFFDA1D1D),
-                                  fontWeight: FontWeight.w500,
+                                  fontSize: 36,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700,
                                 ),
                               ),
-                            ],
-                          ),
-                        ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Padding(
+                        padding: const EdgeInsets.only(top: 250.0),
+                        child: Column(
+                          children: [
+                            const Center(
+                              child: Text(
+                                'CONGRATULATIONS!',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 165),
+                            Center(
+                              child: SizedBox(
+                                height: 300,
+                                child: Image.asset('assets/collected.png',
+                                    fit: BoxFit.contain),
+                              ),
+                            ),
+                            const SizedBox(height: 70),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+        panelBuilder: (controller) => PanelWidget(
+          controller: controller,
+        ),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(50.0),
+          topRight: Radius.circular(50.0),
+        ),
+        ),
+      );
   }
 
   @override
---
+++ lib/ui/views/widgets/panel_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class PanelWidget extends StatelessWidget {
+  final ScrollController controller;
+  const PanelWidget({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) => ListView(
+    padding: const EdgeInsets.symmetric(horizontal: 22.0),
+    controller: controller,
+    children: [
+      Column(
+        children: [
+          Image.asset(
+            'assets/Vector.png',
+          ),
+          const Text(
+            'Let\'s eat!',
+            style: TextStyle(
+              fontSize: 17,
+              color: Color(0xFFDA1D1D),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+      //TO DO: Add the map API
+      Image.asset('assets/Maps.png'),
+    ],
+  );
+}
