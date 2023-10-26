162d8de4a279dd1baaa683672ab73f31fe7dde00
cristopher <emailcristopher@gmail.com>
2023-10-26 13:20:28 +0800

    update the image view in menu section

--- lib/ui/views/details/details_viewmodel.dart
+++ lib/ui/views/details/details_viewmodel.dart
@@ -1,4 +1,5 @@
 import 'package:food_frenzy/app/app.router.dart';
+import 'package:food_frenzy/restaurant_data.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -7,23 +8,19 @@ import '../../../model/details_data.dart';
 
 class DetailsViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
-  List detailsList = [
-    const DetailsData(
-      image: 'https://cloud-cube.s3.amazonaws.com/ck20q033epu8/public/uploads/MI-TSM2-updated.png',
-    ),
-    const DetailsData(
-      image: 'assets/restaurant/product/paeng/2.png',
-    ),
-    const DetailsData(
-      image: 'assets/restaurant/product/paeng/2.png',
-    ),
-    const DetailsData(
-      image: 'assets/restaurant/product/paeng/2.png',
-    ),
-    const DetailsData(
-      image: 'assets/restaurant/product/paeng/2.png',
-    )
-  ];
+
+  List detailsList = [];
+
+  DetailsViewModel() {
+    for (String menuImage in theChosenRestaurant?['menu']) {
+      detailsList.add(
+        DetailsData(
+          image: menuImage,
+        ),
+      );
+    }
+  }
+
   void navigateToRedeem() {
     _navigationService.navigateToNoVouchersView();
   }
--- lib/ui/views/widgets/details_container.dart
+++ lib/ui/views/widgets/details_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../model/details_data.dart';
+import 'package:food_frenzy/restaurant_data.dart';
 
 class DetailsContainer extends StatelessWidget {
   const DetailsContainer({super.key, required this.item});
@@ -16,7 +17,7 @@ class DetailsContainer extends StatelessWidget {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30.0),
           ),
-          child: Image.asset(
+          child: Image.network(
             item.image,
             fit: BoxFit.cover,
           )),
