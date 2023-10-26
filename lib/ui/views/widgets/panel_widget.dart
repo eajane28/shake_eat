16b188904710ea2c664ac31792fd93295175541f
cristopher <emailcristopher@gmail.com>
2023-10-26 15:03:26 +0800

    add restaurant info view

--- lib/ui/views/widgets/panel_widget.dart
+++ lib/ui/views/widgets/panel_widget.dart
@@ -15,7 +15,7 @@ class PanelWidget extends StatelessWidget {
                 'assets/icon/arrow_up.png',
               ),
               const Text(
-                'Let\'s eat!',
+                'More Info',
                 style: TextStyle(
                   fontSize: 17,
                   color: Color(0xFFDA1D1D),
@@ -25,7 +25,64 @@ class PanelWidget extends StatelessWidget {
             ],
           ),
           //TO DO: Add the map API
-          Image.asset('assets/dummy/map.png'),
+          Padding(
+            padding: const EdgeInsets.only(top: 60.0),
+            child: Center(
+              child: Text(
+                "Jolibee alta cita fkj aturas aveiwn askldjf werasf",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(20.0),
+            child: DataTable(
+                columns: [
+                  DataColumn(
+                    label: Text('Description'),
+                  ),
+                  DataColumn(
+                    label: Text(' '),
+                  ),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(Text('Type of Foods')),
+                    DataCell(Text('various')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Average Price')),
+                    DataCell(Text('100 PhP')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Distance')),
+                    DataCell(Text('1 km')),
+                  ])
+                ])
+          ),
+            ElevatedButton(
+              onPressed: () {
+              // Handle the button click here
+              },
+              style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFFDA1D1D),
+              ),
+              child: Text(
+                "Let's go",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+            ),
+            ),
+
         ],
       );
 }
